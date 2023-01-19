ab5a1b5046a131322a580418179c7d7ee884aafc
johan diaz <vasquez.johan@correounivalle.edu.co>
2023-01-19 09:48:41 -0500

    creacion del dashboard con sus componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './assets/css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/Login';
+import Login from './components/Login/index';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Dashboard from './components/Dashboard';
-import Nuevo from './components/Nuevo.jsx';
-import Editar from './components/Editar.jsx'
+import Nuevo from './components/Nuevo/index';
+import Editar from './components/Editar/index'
 
 function App() {
   return (
---
+++ src/components/Dashboard/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+    return (
+        <div>
+            Bienvenido a Home
+        </div>
+    )
+}
+
+export default Home
